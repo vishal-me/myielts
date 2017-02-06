e1bbb428c97659706aa3c9a3c061fff84fec811e
vishal <vishalsharma2830@gmail.com>
2017-02-06 23:52:10 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  comfy_route :cms_admin, :path => '/admin'
+
+  # Make sure this routeset is defined last
+  root to: 'home#index'
+  comfy_route :cms, :path => '/', :sitemap => false
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
